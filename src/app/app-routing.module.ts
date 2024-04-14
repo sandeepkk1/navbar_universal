6ecd645dc044bce223c145fc3c2b60712361bc84
sandeepkk1 <101285062+sandeepkk1@users.noreply.github.com>
2024-04-14 16:17:21 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,67 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { InternshipComponent } from './pages/internship/internship.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
+import { MarketingComponent } from './pages/marketing/marketing.component';
+import { SeoComponent } from './pages/seo/seo.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { TeamMemberComponent } from './pages/team-member/team-member.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"" , component:HomeComponent,
+    
+  },
+  {
+    path:"home" , component:HomeComponent,
+    
+  },
+  {
+    path:"internship" , component:InternshipComponent,
+    
+  },
+  {
+    path:"technology" , component:TechnologyComponent,
+    
+  },
+  {
+    path:"marketing" , component:MarketingComponent,
+    
+  },
+  {
+    path:"seo" , component:SeoComponent,
+    
+  },
+  {
+    path:"service" , component:ServiceComponent,
+    
+  },
+  {
+    path:"portfolio" , component:PortfolioComponent,
+    
+  },
+  {
+    path:"pricing" , component:PricingComponent,
+    
+  },
+  {
+    path:"team" , component:TeamMemberComponent,
+    
+  },
+  {
+    path:"about" , component:AboutComponent,
+    
+  },
+  {
+    path:"contact" , component:ContactComponent,
+    
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InternshipComponent } from './pages/internship/internship.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
+import { MarketingComponent } from './pages/marketing/marketing.component';
+import { SeoComponent } from './pages/seo/seo.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { TeamMemberComponent } from './pages/team-member/team-member.component';
+import { ReviewComponent } from './pages/review/review.component';
+import { ClientComponent } from './pages/client/client.component';
+import { SkillComponent } from './pages/skill/skill.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { PaymentComponent } from './pages/payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    InternshipComponent,
+    TechnologyComponent,
+    MarketingComponent,
+    SeoComponent,
+    ServiceComponent,
+    PortfolioComponent,
+    PricingComponent,
+    TeamMemberComponent,
+    ReviewComponent,
+    ClientComponent,
+    SkillComponent,
+    AboutComponent,
+    ContactComponent,
+    BlogComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
